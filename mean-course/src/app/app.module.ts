c92f7641eb38131b7b08ef2d3e243ea619c11bb0
Gangadhara <gangadhara@bootminds.com>
2018-07-18 17:52:03 +0530

    app module updated

--- mean-course/src/app/app.module.ts
+++ mean-course/src/app/app.module.ts
@@ -1,18 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatToolbarModule } from '@angular/material';
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
-  imports: [
+  imports: [ 
     BrowserModule,
     FormsModule,
     ReactiveFormsModule,
-    BrowserAnimationsModule
+    HttpClientModule,
+    BrowserAnimationsModule,
+    CommonModule,
+    MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
