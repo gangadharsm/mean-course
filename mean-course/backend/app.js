400a544e1c65d2379ecf46018397310eca13c61a
Gangadhara <gangadhara@bootminds.com>
2018-07-19 17:52:19 +0530

    Implemented the posts api

--- mean-course/backend/app.js
+++ mean-course/backend/app.js
@@ -2,13 +2,19 @@ const express = require('express');
 
 const app = express();
 
-app.use((req, res , next) => {
-    console.log('First middleware');
-    next();
+app.use('/api/posts',(req, res , next) => {
+    const posts = [
+        {
+            id:"1",
+            title: "first title",
+            content: "first content"
+        }
+    ]
+    res.status(200).json({
+        message: 'Posts fetched successfully!',
+        posts: posts
+    });
 });
 
-app.use((req, res, next) => {
-    res.send('Hello from express');
-});
 
 module.exports = app
