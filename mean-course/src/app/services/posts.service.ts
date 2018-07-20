063c8f6bda4e298e9e8a486dbb0c573c90ac1de7
Gangadhara <gangadhara@bootminds.com>
2018-07-20 21:10:04 +0530

    post methed implemented

--- mean-course/src/app/services/posts.service.ts
+++ mean-course/src/app/services/posts.service.ts
@@ -1,9 +1,38 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
+
+import { Subject } from 'rxjs';
+import { Post } from '../models/post';
+import { post } from 'selenium-webdriver/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PostsService {
+  private posts: Post[] = [];
+  private postsUpdated = new Subject<Post[]>();
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getPosts() { 
+    this.http.get<{message:string, posts: Post[]}>('http://localhost:3000/api/posts')
+    .subscribe((postData)=>{
+      this.posts = postData.posts;
+      this.postsUpdated.next([...this.posts]);
+    });
+  }
+
+  getPostUpdateListener() {
+    return this.postsUpdated.asObservable();
+  }
+
+  addPost(title: string, content: string) {
+    const post: Post= {id: null, title: title, content: content};
+    this.http.post<{message: string}>("http://localhost:3000/api/posts",post)
+    .subscribe(responseDate => {
+      console.log(responseDate.message);
+      this.posts.push(post);
+      this.postsUpdated.next([...this.posts]);
+    });
+  }
 }
