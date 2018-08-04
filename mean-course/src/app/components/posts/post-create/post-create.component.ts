81080dc80e68074087a61479e7c9adb9bce83114
Gangadhara <gangadhara@bootminds.com>
2018-08-04 19:53:00 +0530

    image preview implemented

--- mean-course/src/app/components/posts/post-create/post-create.component.ts
+++ mean-course/src/app/components/posts/post-create/post-create.component.ts
@@ -16,6 +16,7 @@ export class PostCreateComponent implements OnInit {
   post: Post;
   form: FormGroup
   isLoading: boolean = false;
+  imagePreview: string;
   constructor(public postsService: PostsService, public route: ActivatedRoute) { }
 
   ngOnInit() {
@@ -23,7 +24,8 @@ export class PostCreateComponent implements OnInit {
       title: new FormControl(null, {
         validators: [Validators.required, Validators.minLength(3)]
       }),
-      content: new FormControl(null, { validators: [Validators.required] })
+      content: new FormControl(null, { validators: [Validators.required] }),
+      image: new FormControl(null, {validators: [Validators.required]})
     })
     this.route.paramMap.subscribe((paramMap: ParamMap) => {
       if (paramMap.has("postId")) {
@@ -43,6 +45,19 @@ export class PostCreateComponent implements OnInit {
         this.postId = null;
       }
     });
+  }
+  
+  onImagePicked(event: Event) {
+    const file = (event.target as HTMLInputElement).files[0];
+    this.form.patchValue({
+      image: file
+    });
+    this.form.get('image').updateValueAndValidity();
+    const reader = new FileReader();
+    reader.onload = () => {
+      this.imagePreview = reader.result;
+    };
+    reader.readAsDataURL(file);
   }
 
   onSavePost() {
