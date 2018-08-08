739d8d264891ac110dc8625ab86022f4e8b85306
Gangadhara <gangadhara@bootminds.com>
2018-08-08 12:13:43 +0530

    auth service create user function added

--- mean-course/src/app/services/auth-service.service.ts
+++ mean-course/src/app/services/auth-service.service.ts
@@ -1,9 +1,14 @@
 import { Injectable } from '@angular/core';
-
+import { HttpClient } from '@angular/common/http';
+import { AuthData } from '../models/auth-data'
 @Injectable({
   providedIn: 'root'
 })
 export class AuthServiceService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  createUser(email: string, password: string) {
+    const authData: AuthData = {email: email, password: password}
+  }
 }
