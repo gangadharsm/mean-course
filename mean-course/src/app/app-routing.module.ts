4e7a85186206875378fc1ff70b34dc5011c53c31
Gangadhara <gangadhara@bootminds.com>
2018-08-08 16:34:58 +0530

    signup and login ui changes

--- mean-course/src/app/app-routing.module.ts
+++ mean-course/src/app/app-routing.module.ts
@@ -2,14 +2,18 @@ import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from '@angular/router';
 import { PostListComponent } from "./components/posts/post-list/post-list.component";
 import { PostCreateComponent } from "./components/posts/post-create/post-create.component";
+import { LoginComponent } from "./components/auth/login/login.component";
+import { SignupComponent } from "./components/auth/signup/signup.component";
 
 const routes: Routes = [
     { path: '', component: PostListComponent },
     { path: 'create', component: PostCreateComponent },
-    { path: 'edit/:postId', component: PostCreateComponent }
+    { path: 'edit/:postId', component: PostCreateComponent },
+    { path: 'login', component: LoginComponent },
+    { path: 'signup', component: SignupComponent }
 ]
 @NgModule({
-    imports: [RouterModule.forRoot(routes, {scrollPositionRestoration: 'enabled'})],
+    imports: [RouterModule.forRoot(routes, { scrollPositionRestoration: 'enabled' })],
     exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- mean-course/src/app/components/auth/login/login.component.ts
+++ mean-course/src/app/components/auth/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-
+  isLoading: boolean = false;
   constructor() { }
 
   ngOnInit() {
   }
-
+  
 }
