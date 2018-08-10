d1ab8392352328839b46a66759959d006a33ed0c
Gangadhara <gangadhara@bootminds.com>
2018-08-10 17:22:41 +0530

    Adding Auth guard

--- mean-course/src/app/app-routing.module.ts
+++ mean-course/src/app/app-routing.module.ts
@@ -4,16 +4,18 @@ import { PostListComponent } from "./components/posts/post-list/post-list.compon
 import { PostCreateComponent } from "./components/posts/post-create/post-create.component";
 import { LoginComponent } from "./components/auth/login/login.component";
 import { SignupComponent } from "./components/auth/signup/signup.component";
+import { AuthGuard } from "./services/auth.guard";
 
 const routes: Routes = [
     { path: '', component: PostListComponent },
-    { path: 'create', component: PostCreateComponent },
-    { path: 'edit/:postId', component: PostCreateComponent },
+    { path: 'create', component: PostCreateComponent, canActivate: [AuthGuard] },
+    { path: 'edit/:postId', component: PostCreateComponent, canActivate: [AuthGuard] },
     { path: 'login', component: LoginComponent },
     { path: 'signup', component: SignupComponent }
 ]
 @NgModule({
     imports: [RouterModule.forRoot(routes, { scrollPositionRestoration: 'enabled' })],
-    exports: [RouterModule]
+    exports: [RouterModule],
+    providers: [AuthGuard]
 })
 export class AppRoutingModule { }
