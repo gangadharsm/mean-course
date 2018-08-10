b4a92e6e5c16b83795e66a458041254ba384c3ea
Gangadhara <gangadhara@bootminds.com>
2018-08-10 15:36:12 +0530

    Improving the UI Header to Reflect the Authentication Status

--- mean-course/src/app/components/header/header.component.ts
+++ mean-course/src/app/components/header/header.component.ts
@@ -1,15 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { AuthService } from '../../services/auth.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent implements OnInit {
-
-  constructor() { }
+export class HeaderComponent implements OnInit, OnDestroy {
+  userIsAuthenticated: boolean = false;
+  private authListenerSubs: Subscription;
+  constructor(private authService: AuthService) { }
 
   ngOnInit() {
+    this.authListenerSubs = this.authService.getAuthStatusListener()
+      .subscribe(isAuthenticated => {
+        this.userIsAuthenticated = isAuthenticated;
+      });
   }
 
+  ngOnDestroy() {
+    this.authListenerSubs.unsubscribe();
+  }
 }
--- mean-course/src/app/services/auth.service.ts
+++ mean-course/src/app/services/auth.service.ts
@@ -1,33 +1,39 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { AuthData } from '../models/auth-data';
+import { Subject } from 'rxjs';
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
   private token: string;
+  private authStatusListener = new Subject<boolean>();
   constructor(private http: HttpClient) { }
 
   getToken() {
     return this.token;
   }
 
+  getAuthStatusListener() {
+    return this.authStatusListener.asObservable();
+  }
 
   createUser(email: string, password: string) {
-    const authData: AuthData = {email: email, password: password};
+    const authData: AuthData = { email: email, password: password };
     this.http.post('http://localhost:3000/api/user/signup', authData)
-    .subscribe(response => {
-      console.log(response);
-    });
+      .subscribe(response => {
+        console.log(response);
+      });
   }
 
   login(email: string, password: string) {
-    const authData: AuthData = {email: email, password: password};
-    this.http.post<{token: string}>('http://localhost:3000/api/user/login', authData)
-    .subscribe(response => {
-      const token = response.token;
+    const authData: AuthData = { email: email, password: password };
+    this.http.post<{ token: string }>('http://localhost:3000/api/user/login', authData)
+      .subscribe(response => {
+        const token = response.token;
         this.token = token;
-    });
+        this.authStatusListener.next(true);
+      });
   }
 }
