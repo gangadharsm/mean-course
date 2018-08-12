5a13650de4cff44f2756b069504db963268e9fc8
Gangadhara <gangadhara013@gmail.com>
2018-08-12 12:21:08 +0530

    back end url added into the environment file

--- mean-course/src/app/services/auth.service.ts
+++ mean-course/src/app/services/auth.service.ts
@@ -2,7 +2,10 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { AuthData } from '../models/auth-data';
 import { Subject } from 'rxjs';
-import { Router } from '@angular/router';
+import { Router } from '@angular/router'; 
+import { environment }  from '../../environments/environment';
+
+const BACKEND_URL = environment.apiUrl+ "/user/"
 @Injectable({
   providedIn: 'root'
 })
@@ -33,7 +36,7 @@ export class AuthService {
 
   createUser(email: string, password: string) {
     const authData: AuthData = { email: email, password: password };
-     this.http.post('http://localhost:3000/api/user/signup', authData)
+     this.http.post(BACKEND_URL +'signup', authData)
       .subscribe(() => {
         this.router.navigate(['/']);
       }, error => {
@@ -43,7 +46,7 @@ export class AuthService {
 
   login(email: string, password: string) {
     const authData: AuthData = { email: email, password: password };
-    this.http.post<{ token: string, expiresIn: number, userId: string }>('http://localhost:3000/api/user/login', authData)
+    this.http.post<{ token: string, expiresIn: number, userId: string }>(BACKEND_URL + 'login', authData)
       .subscribe(response => {
         const token = response.token;
         this.token = token;
